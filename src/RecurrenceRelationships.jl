a78d23c0b7e44afac89963ebaff4f4f8fdcbb572
Sheehan Olver <solver@mac.com>
2024-08-13 13:33:48 +0100

    add extra

--- src/RecurrenceRelationships.jl
+++ src/RecurrenceRelationships.jl
@@ -5,4 +5,10 @@ export forwardrecurrence, forwardrecurrence!, clenshaw, clenshaw!
 include("forward.jl")
 include("clenshaw.jl")
 
+if !isdefined(Base, :get_extension)
+    include("../ext/RecurrenceRelationshipsFillArraysExt.jl")
+    include("../ext/RecurrenceRelationshipsLazyArraysExt.jl")
+end
+
+
 end # module RecurrenceRelationships
