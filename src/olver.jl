2d29fd80695a78455031438289672f925d2eeeac
Sheehan Olver <solver@mac.com>
2024-08-15 16:55:06 +0100

    Fix Olver's algorithm error bounds (#7)
    * Fix Olver's algorithm error bounds
    
    * adjust arguments for olver
    
    * split out logic (for potentially overriding)
    
    * increase cov
    
    * Check error bound is achieved
    
    * Correct error formula
    
    * Update runtests.jl
    
    * Update runtests.jl
    
    * Update runtests.jl

--- src/olver.jl
+++ src/olver.jl
@@ -1,26 +1,68 @@
-function olver!(d::AbstractVector{T}, r, a, b, c; atol=eps(T)) where T
-    resize!(r, max(length(r), 10))
 
-    r[1] = b[1]/c[1]
+
+Base.@propagate_inbounds @inline function olver_forward_next(d, r, a, b, c, f, k)
+    r̃ₖ = inv(r[k-1])
+    f[k] - a[k-1]d[k-1]r̃ₖ, b[k] - c[k-1]a[k-1]r̃ₖ
+end
+
+Base.@propagate_inbounds @inline olver_backward_next(d, r, c, k) = (d[k]-c[k]*d[k+1])/r[k]
+
+
+function olver_forward!(d::AbstractVector{T}, r, a, b, c, f; atol=eps(T)) where T
+    N = length(d) # compute at least `N` entries
+    Ñ = min(2N+10,length(f))
+    resize!(r, Ñ)
+    resize!(d, Ñ)
+
+    r[1] = b[1]
     M = zero(T)
     # d[1] is left unmodified
-    for k = 2:length(d)
-        r[k] = b[k] - c[k-1]a[k-1]/r[k-1]
-        d[k] -= a[k-1]d[k-1]/r[k-1]
-        M = max(M,one(T)) / abs(r[k])
-        if M * abs(d[k]) ≤ atol
-            resize!(d, k)
-            break
-        end
+    d[1] = f[1]
+    for k = 2:N
+        d[k],r[k] = olver_forward_next(d, r, a, b, c, f, k)
+        M = max(M*c[k-1],one(T)) / abs(r[k])
     end
 
+    for k = N+1:length(f)
+        if k > length(r)
+            Ñ = min(2length(r),length(f))
+            resize!(r, Ñ)
+            resize!(d, Ñ)
+        end
+        d[k],r[k] = olver_forward_next(d, r, a, b, c, f, k)
+        M = max(M*c[k-1],one(T)) / abs(r[k])
+        ε = M * abs(d[k])
+        if ε ≤ atol
+            return resize!(d,k-1), resize!(r,k), ε
+        end
+    end
+    d, r, zero(T)
+end
+
+function olver!(d::AbstractVector{T}, r, a, b, c, f; atol=eps(T)) where T
+    olver_forward!(d, r, a, b, c, f; atol)
     # backsubstitution
     N = length(d)
-    d[N] /= -r[N]
-    for k = length(d)-1:-1:1
-        d[k] = (d[k]-c[k]*d[k+1])/r[k]
+    d[N] /= r[N]
+    for k = N-1:-1:1
+        d[k] = olver_backward_next(d, r, c, k)
     end
     d
 end
 
-olver(d::AbstractVector{T}, a, b, c; atol=eps(float(T))) where T = olver!(convert(AbstractVector{float(T)}, Base.copymutable(d)), similar(d), a, b, c; atol)
+
+"""
+    olver(a, b, c, f, N=1; atol)
+
+returns a vector `u` satisfying the 3-term recurrence relationship
+
+    -b[1]u[1] + c[1]u[2] = f[1]
+    a[k-1]u[k-1] - b[k]u[k] + c[k]u[k+1] = f[k]
+
+It will compute at least `N` entries, but possibly more, returning the result
+when the backward error between consective truncations is less than `atol`.
+"""
+function olver(a, b, c, f::AbstractVector{T}, N=1; atol=eps(float(T))) where T
+    dest = Vector{float(T)}(undef, N)
+    olver!(dest, similar(dest), a, b, c, f; atol)
+end
