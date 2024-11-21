911622e48b3ea193ec3ff6a4d3d7bd1e81a9edb5
Sheehan Olver <solver@mac.com>
2024-11-21 15:30:56 +0000

    Drop old Julia support (#11)

--- src/RecurrenceRelationships.jl
+++ src/RecurrenceRelationships.jl
@@ -5,11 +5,4 @@ include("forward.jl")
 include("clenshaw.jl")
 include("olver.jl")
 
-if !isdefined(Base, :get_extension)
-    include("../ext/RecurrenceRelationshipsLinearAlgebraExt.jl")
-    include("../ext/RecurrenceRelationshipsFillArraysExt.jl")
-    include("../ext/RecurrenceRelationshipsLazyArraysExt.jl")
-end
-
-
 end # module RecurrenceRelationships
