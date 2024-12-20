8da278893a7f41d90d96d90ac30d8e1bc8c080e3
Sheehan Olver <solver@mac.com>
2024-12-20 10:34:01 +0000

    Make polynomialtype take just coeffs and var types

--- src/forward.jl
+++ src/forward.jl
@@ -48,10 +48,10 @@ where `A`, `B`, and `C` are `AbstractVector`s containing the first form recurren
 i.e. it returns 
 """
 forwardrecurrence(N::Integer, A::AbstractVector, B::AbstractVector, C::AbstractVector, x) =
-    forwardrecurrence!(Vector{polynomialtype(eltype(A),eltype(B),eltype(C),typeof(x))}(undef, N), A, B, C, x)
+    forwardrecurrence!(Vector{polynomialtype(promote_type(eltype(A),eltype(B),eltype(C)),typeof(x))}(undef, N), A, B, C, x)
 
 forwardrecurrence(N::Integer, A::AbstractVector, B::AbstractVector, C::AbstractVector) =
-    forwardrecurrence!(Vector{polynomialtype(eltype(A),eltype(B),eltype(C))}(undef, N), A, B, C)
+    forwardrecurrence!(Vector{promote_type(eltype(A),eltype(B),eltype(C))}(undef, N), A, B, C)
 
 
 forwardrecurrence(A::AbstractVector, B::AbstractVector, C::AbstractVector, x...) = forwardrecurrence(min(length(A), length(B), length(C)), A, B, C, x...)
